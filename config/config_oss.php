29f976ce541ac09ed920e67238ec59d6e9eed0cb
mentallxm <strawer@live.cn>
2017-09-15 14:15:49 +0800

    find config

---
+++ config/config_oss.php
@@ -0,0 +1,18 @@
+<?php
+
+$config->oss = [
+    // 是否开启oss
+    'on' => true,
+    // 挂载点
+    'endpoint' => '',
+    // accessKeyId
+    'accessKeyId' => '',
+    // accessKeySecret
+    'accessKeySecret' => '',
+    // bucket
+    'bucket' => '',
+    // domain
+    'baseroot' => '',
+    // 上传目标目录
+    'dir' => ''
+];
